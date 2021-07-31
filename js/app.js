b31261891af7dae61243eccd261de73c0a05cf17
Robert Grüning <r.gruening@web.de>
2021-08-01 00:26:23 +0200

    #1 Anfängermodus implementiert

--- js/app.js
+++ js/app.js
@@ -1,16 +1,77 @@
+//#region variables
 let _currentGame = null;
 var _currentPlace = null;
 var _foundItemNames = new Array();
-var _viewMode = "default";
+var _actionMode = "default";
+var _levelOfDifficulty = "beginner";
+//#endregion
+
+//#region properties
+function initFoundItemName() {
+	_foundItemNames = new Array();
+}
+
+function addFoundItemName(itemName) {
+	_foundItemNames.push(itemName);
+}
+
+function containsFoundItemByName(name) {
+	var itemNames = _foundItemNames.filter(function(itemName, index) {
+		return itemName == name;
+	});
+
+	return itemNames.length >= 1;
+}
+
+function getFoundItemName() {
+	return _foundItemNames;
+}
+
+function setActionMode(actionMode) {
+	_actionMode = actionMode;
+}
+
+/**
+ * Returns the action mode.
+ * @returns "default"|"view"|"take"
+ */
+function getActionMode() {
+	return _actionMode;
+}
+
+function setlevelOfDifficulty(levelOfDifficulty) {
+	_levelOfDifficulty = levelOfDifficulty;
+}
+
+/**
+ * Returns the level of difficulty.
+ * @returns "beginner"|"expert"
+ */
+function getLevelOfDifficulty() {
+	return _levelOfDifficulty;
+}
+//#endregion
 
 $(document).ready(function() {
+	$("#button-go-forward").click(function() { goToDirection("forward"); });
+	$("#button-go-left").click(function() { goToDirection("left"); });
+	$("#button-go-right").click(function() { goToDirection("right"); });
+	$("#button-go-back").click(function() { goToDirection("back"); });
 
 	$("#button-open-menu").click(function() { $("#modal-menu").show(); });
 	$("#button-close-menu").click(function() { $("#modal-menu").hide(); });
 
 	$("#button-open-about").click(function() {
 		$("#modal-menu").hide();
-		$("#modal-about").show();
+		$("#contributors").empty();
+
+		_currentGame.contributors.forEach(function(item) {
+			var li = $("<li></li>");
+			li.html(item);
+			$("#contributors").append(li);
+		});
+
+		$("#modal-about").show();		
 	});
 	$("#button-close-about").click(function() { $("#modal-about").hide(); });
 
@@ -19,16 +80,17 @@ $(document).ready(function() {
 		$("#modal-help").show();
 	});
 	$("#button-close-help").click(function() { $("#modal-help").hide(); });
-	$("#button-new-game").click(startNewGame);
-
-	$("#button-close-map").click(function() { show(_currentPlace); });
+	$("#button-new-game").click(function() {
+		$("#modal-menu").hide();
+		startNewGame();
+	});
 
 	$("#button-open-quests").click(openQuests);
 	$("#button-close-quests").click(function() { $("#modal-quests").hide(); });
 
-	$("#button-view").click(function() { switchMode("view"); });
-	$("#button-take").click(function() { switchMode("take"); });
-	$("#button-dig").click(function() {  });
+	$("#button-view").click(function() { switchActionMode(getActionMode() == "view" ? "default" : "view"); });
+	$("#button-take").click(function() { switchActionMode(getActionMode() == "take" ? "default" : "take"); });
+	$("#button-dig").click(function() { dig(); });
 
 	$("#button-close-item-info").click(function() { $("#modal-item-info").hide(); });
 
@@ -37,7 +99,7 @@ $(document).ready(function() {
 	initFirstGame();
 });
 
-function switchMode(newMode) {
+function switchActionMode(newActionMode) {
 	// reset view
 	$("#view").removeClass("mode__default");
 	$("#view").removeClass("mode__view");
@@ -47,43 +109,36 @@ function switchMode(newMode) {
 	$(".viewable").off("click");
 	$(".takable").off("click");
 
-	// reset button view
-	$("#button-view").removeClass("w3-indigo");
-	$("#button-view").addClass("w3-pale-blue");
-	$("#button-view").addClass("w3-hover-indigo");
+	// reset active button
+	$("#button-view").removeClass("action-button__active");
+	$("#button-take").removeClass("action-button__active");
 
-	// reset button take
-	$("#button-take").removeClass("w3-indigo");
-	$("#button-take").addClass("w3-pale-blue");
-	$("#button-take").addClass("w3-hover-indigo");
-
-	if (newMode == "view" &&
-		_viewMode != "view") {
-		$("#button-view").removeClass("w3-pale-blue");
-		$("#button-view").addClass("w3-indigo");
+	if (newActionMode == "view" ||
+		(newActionMode == "default" &&
+		 getLevelOfDifficulty() == "beginner")) {
+			 
+		$("#button-view").addClass("action-button__active");
 		$("#view").addClass("mode__view");
 		$(".viewable.item").click(function() { openItemInfo($(this).attr("item-name")); });
 		$(".viewable.object").click(function() { openObjectInfo($(this).attr("object-name")); });
-		_viewMode = "view";
+		setActionMode("view");
 	}
-	else if (newMode == "take" &&
-			_viewMode != "take") {
-		$("#button-take").removeClass("w3-pale-blue");
-		$("#button-take").addClass("w3-indigo");
+	else if (newActionMode == "take") {
+		$("#button-take").addClass("action-button__active");
 		$("#view").addClass("mode__take");
 		$(".takable").click(function() { takeItem($(this).attr("item-name")); });
-		_viewMode = "take";
+		setActionMode("take");
 	}
 	else {
 		$("#view").addClass("mode__default");
 		$(".viewable.item").click(function() { openItemInfo($(this).attr("item-name")); });
 		$(".viewable.object").click(function() { openObjectInfo($(this).attr("object-name")); });
-		_viewMode = "default";
+		getActionMode("default");
 	}
 }
 
 function initFirstGame() {
-	_foundItemNames = new Array();
+	initFoundItemName();
 	_currentGame = _config;
 
 	initItemInventory();
@@ -92,7 +147,7 @@ function initFirstGame() {
 }
 
 function startNewGame() {
-	_foundItemNames = new Array();
+	initFoundItemName();
 	_currentGame = _config;
 
 	initItemInventory();
@@ -192,22 +247,16 @@ function findObjectByName(name) {
 	return objects[0];
 }
 
-function containsFoundItemByName(name) {
-	var itemNames = _foundItemNames.filter(function(itemName, index) {
-		return itemName == name;
-	});
-
-	return itemNames.length >= 1;
-}
-
 function openMap() {
 	$("#button-go-forward").hide();
 	$("#button-go-right").hide();
 	$("#button-go-left").hide();
 	$("#button-go-back").hide();
-	$("#img-view").attr("src", "images/map.jpg");
-
 	disableButtonOpenMap();
+	$("#button-view").hide();
+	$("#button-take").hide();
+	$("#button-dig").hide();
+	$("#img-view").attr("src", "images/map.jpg");
 
 	$("#markers").empty();
 	$("#objects").empty();
@@ -215,12 +264,11 @@ function openMap() {
 
 	_currentGame.places.forEach(function(place, index) {
 		if (typeof place.position !== "undefined") {
-			let radius = 8;
+			let radius = 15;
 			let div = $("<div></div>");
 			div.addClass("w3-display-topleft");
 			div.addClass("w3-circle");
-			div.addClass("w3-pale-blue");
-			div.addClass("w3-hover-indigo");
+			div.addClass("map--marker");
 			div.css({
 				"margin-top": place.position.top - radius,
 				"margin-left": place.position.left - radius,
@@ -232,6 +280,10 @@ function openMap() {
 			$("#markers").append(div);
 		}
 	});
+}
+
+function closeMap() {
+	enableButtonOpenMap();
 }
 
 function openQuests() {
@@ -281,17 +333,24 @@ function openObjectInfo(objectName) {
 }
 
 function takeItem(itemName) {
-	_foundItemNames.push(itemName);
+	addFoundItemName(itemName);
 	$("#item" + itemName).remove();
 	initItemInventory();
-	switchMode("default");
+	switchActionMode("default");
 
 	let currentPlace = findPlaceByName(_currentPlace);
 	currentPlace.items.splice(currentPlace.items.indexOf(itemName), 1);
 
 	if (typeof currentPlace.dig !== "undefined" &&
 		currentPlace.items.length == 0) {
-		enableButtonDig();
+		$("#button-dig").show();
+	}
+
+	if (currentPlace.items.length == 0) {
+		$("#button-take").hide();
+	}
+	else {
+		$("#button-take").show();
 	}
 }
 
@@ -299,54 +358,66 @@ function show(placeName) {
 	$("#markers").empty();
 	$("#objects").empty();
 	$("#items").empty();
-	$("#button-go-forward").show();
-	$("#button-go-right").show();
-	$("#button-go-left").show();
-	$("#button-go-back").show();
-
-	disableButtonGoForward();
-	disableButtonGoRight();
-	disableButtonGoLeft();
-	disableButtonGoBack();
-	disableButtonDig();
+	$("#button-go-forward").hide();
+	$("#button-go-right").hide();
+	$("#button-go-left").hide();
+	$("#button-go-back").hide();
+	
+	enableButtonOpenMap();
+	$("#button-view").hide();
+	$("#button-take").hide();
+	$("#button-dig").hide();
 
 	var place = findPlaceByName(placeName);
 	_currentPlace = place.name;
+	
+	if (typeof place.description === "undefined") {
+		$("#scene-description").html("");
+	}
+	else {
+		$("#scene-description").html(place.description);
+	}
+
 	$("#img-view").attr("src", "images/" + place.name + ".jpg");
 
 
 	if (typeof place.forward !== "undefined") {
-		enableButtonGoForward();
+		$("#button-go-forward").show();
 	}
 
 	if (typeof place.right !== "undefined") {
-		enableButtonGoRight();
+		$("#button-go-right").show();
 	}
 
 	if (typeof place.left !== "undefined") {
-		enableButtonGoLeft();
+		$("#button-go-left").show();
 	}
 
 	if (typeof place.back !== "undefined") {
-		enableButtonGoBack();
+		$("#button-go-back").show();
 	}
 
-	if (typeof place.items !== "undefined") {
+	if (typeof place.items !== "undefined" &&
+		place.items.length >= 1) {
 		createItems(place.items);
+		$("#button-take").show();
+		$("#button-view").show();
 	}
 
-	if (typeof place.objects !== "undefined") {
+	if (typeof place.objects !== "undefined" &&
+		place.objects.length >= 1) {
 		createObjects(place.objects);
+		$("#button-view").show();
 	}
 
 	if (typeof place.dig !== "undefined" &&
 		(typeof place.items === "undefined" ||
 		 place.items.length == 0)) {
-		enableButtonDig();
+		$("#button-dig").show();
 	}
 
 	enableButtonOpenMap();
-	switchMode("default");
+	switchActionMode("default");
 }
 
 function createItems(itemNames) {
@@ -414,99 +485,19 @@ function enableButtonOpenMap() {
 	$("#button-open-map").click(openMap);
 	$("#button-open-map").removeClass("disabled");
 	$("#button-open-map").prop("disabled", false);
-
-	$("#button-close-map").hide();
 }
 
 function disableButtonOpenMap() {
 	$("#button-open-map").off("click");
 	$("#button-open-map").addClass("disabled");
 	$("#button-open-map").prop("disabled", true);
-
-	if (_currentPlace != null) {
-		$("#button-close-map").show();
-	}
-}
-
-function setEnabledButtonCss(button) {
-	button.removeClass("disabled");
-	button.prop("disabled", false);
-	button.removeClass("w3-gray");
-	button.removeClass("w3-hover-gray");
-	button.removeClass("w3-text-gray");
-	button.removeClass("w3-hover-text-gray");
-	button.addClass("w3-pale-blue");
-	button.addClass("w3-hover-indigo");
-	button.addClass("w3-text-black");
-	button.addClass("w3-hover-text-white");
 }
 
 function disableButton(button) {
 	button.off("click");
 	button.addClass("disabled");
+	button.addClass("w3-disabled");
 	button.prop("disabled", true);
-	button.removeClass("w3-pale-blue");
-	button.removeClass("w3-hover-indigo");
-	button.removeClass("w3-text-black");
-	button.removeClass("w3-hover-text-white");
-	button.addClass("w3-gray");
-	button.addClass("w3-hover-gray");
-	button.addClass("w3-text-gray");
-	button.addClass("w3-hover-text-gray");
-}
-
-function disableButtonGoForward() {
-	disableButton($("#button-go-forward"));
-}
-
-function enableButtonGoForward() {
-	$("#button-go-forward").off("click");
-	$("#button-go-forward").click(function() { goToDirection("forward"); });
-	setEnabledButtonCss($("#button-go-forward"));
-}
-
-function disableButtonGoForward() {
-	disableButton($("#button-go-forward"));
-}
-
-function enableButtonGoLeft() {
-	$("#button-go-left").off("click");
-	$("#button-go-left").click(function() { goToDirection("left"); });
-	setEnabledButtonCss($("#button-go-left"));
-}
-
-function disableButtonGoLeft() {
-	disableButton($("#button-go-left"));
-}
-
-function enableButtonGoRight() {
-	$("#button-go-right").off("click");
-	$("#button-go-right").click(function() { goToDirection("right"); });
-	setEnabledButtonCss($("#button-go-right"));
-}
-
-function disableButtonGoRight() {
-	disableButton($("#button-go-right"));
-}
-
-function enableButtonGoBack() {
-	$("#button-go-back").off("click");
-	$("#button-go-back").click(function() { goToDirection("back"); });
-	setEnabledButtonCss($("#button-go-back"));
-}
-
-function disableButtonGoBack() {
-	disableButton($("#button-go-back"));
-}
-
-function enableButtonDig() {
-	$("#button-dig").off("click");
-	$("#button-dig").click(function() { dig(); });
-	setEnabledButtonCss($("#button-dig"));
-}
-
-function disableButtonDig() {
-	disableButton($("#button-dig"));
 }
 
 function goToDirection(direction) {
